0a5486734e27127e278c75e9c2a14d650975d741
netalmkn <netalmkn@gmail.com>
2025-10-31 21:03:57 +0200

    Completed assignments

--- FVCalc.java
+++ FVCalc.java
@@ -3,8 +3,11 @@ public class FVCalc {
         int currentValue = Integer.parseInt(args[0]);
         double rate = Double.parseDouble(args[1]);
         int years = Integer.parseInt(args[2]);
+
         double futureValue = currentValue * Math.pow(1 + rate / 100, years);
+
         System.out.println("After " + years + " years, $" + currentValue +
                            " saved at " + rate + "% will yield $" + (int)Math.round(futureValue));
     }
 }
+
