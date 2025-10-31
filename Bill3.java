9a606f52fb0fd60201487022bb8ca0a957398055
netalmkn <netalmkn@gmail.com>
2025-10-31 20:52:17 +0200

    final assignments

--- Bill3.java
+++ Bill3.java
@@ -1,13 +1,10 @@
-// Splits a restaurant bill evenly among three diners.
 public class Bill3 {
     public static void main(String[] args) {
         String name1 = args[0];
         String name2 = args[1];
         String name3 = args[2];
-        int totalAmount = Integer.parseInt(args[3]);
-
-        double sumEach = Math.ceil(totalAmount / 3.0);
-        System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 +
-                           ": pay " + sumEach + " Shekels each.");
+        int total = Integer.parseInt(args[3]);
+        double each = Math.ceil(total / 3.0);
+        System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + each + " Shekels each");
     }
 }
--- NumWords.java
+++ NumWords.java
@@ -4,7 +4,6 @@ public class NumWords {
         int hundreds = num / 100;
         int tens = (num / 10) % 10;
         int ones = num % 10;
-
         System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
     }
 }
